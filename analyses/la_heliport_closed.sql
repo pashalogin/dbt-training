7644a3ae5aa806214923060bd808103e7319e83c
Mastan Pasha Shaik <pshaik@phdata.io>
2026-05-12 20:48:22 +0000

    test1234

--- analyses/la_heliport_closed.sql
+++ analyses/la_heliport_closed.sql
@@ -1,7 +1,7 @@
 -- Validate that the SCD snapshot captured the closure of
 -- Los Angeles County Sheriff's Department Heliport (01CN).
 -- Run with: dbt show --select la_heliport_closed
--- This is test
+-- This is test 123
 select
     airport_ident,
     airport_name,
